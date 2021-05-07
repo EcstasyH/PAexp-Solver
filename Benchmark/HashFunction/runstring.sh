341588e7fc8fee9fadb7801905be0e0e663d7b7f
EcstasyH <wuhao164@mails.ucas.ac.cn>
2021-05-07 16:43:15 +0800

    generate string tests

--- Benchmark/HashFunction/runstring.sh
+++ Benchmark/HashFunction/runstring.sh
@@ -3,7 +3,7 @@ echo "--------------------------------"
 
 echo "begin string test Using Z3 ..."
 START=$(date +%s)
-z3 -smt2 string -T:120
+z3 -smt2 ./all/head/test1 -T:60
 END=$(date +%s)
 DIFF=$(( $END - $START ))
 echo "Z3 took $DIFF seconds"
@@ -12,7 +12,7 @@ echo "--------------------------------"
 
 echo "begin string test Using CVC4 ..."
 START=$(date +%s)
-cvc4 string -L smt --tlimit=120000 --strings-exp   
+cvc4 ./all/head/test1 -L smt --tlimit=60000 --strings-exp   
 END=$(date +%s)
 DIFF=$(( $END - $START ))
 echo "CVC4 took $DIFF seconds"
