40c2b1a4f4d359e2049dd4ec14a70be642582b88
EcstasyH <wuhao164@mails.ucas.ac.cn>
2021-05-17 15:54:47 +0800

    Z3&mysolver result on Arith

--- Benchmark/NIA-test/produce.py
+++ Benchmark/NIA-test/produce.py
@@ -1,15 +1,15 @@
 from z3 import *
 import random
 
-expnum = 4
-linearnum = 5
-expfml = 5
-linearfml = 6
+expnum = 1
+linearnum = 2
+expfml = 2
+linearfml = 3
 totaltests = 100
 
 x=[0]*(expnum+linearnum+2)
 
-g = open("new-test/4-5-5-6/paras","w+")
+g = open("new-test/1-2-2-3/paras","w+")
 g.write("%d\n" %totaltests)
 g.write("{%d,%d,%d,%d}\n" %(expnum,linearnum,expfml,linearfml))
 
@@ -52,7 +52,7 @@ for testno in range(1,totaltests+1):
         g.write("{")
         expr = 0
         for j in range (1,expnum+1):
-            coef = random.randint(-10,10)
+            coef = random.randint(-100,100)
             expr = expr + coef * pow(x[j])
             g.write("%d," %coef)
         # bound cases
@@ -69,14 +69,14 @@ for testno in range(1,totaltests+1):
         expr = 0
         g.write("{")
         for j in range (1,expnum+linearnum+1):
-            coef = random.randint(-10**2,10**2)
+            coef = random.randint(-10**5,10**5)
             expr = expr + coef * x[j]
             g.write("%d," %coef)
         num = random.randint(-10**5,10**5)
         s.add(expr <= num)
         g.write("%d}\n" % num)
             
-    f = open('new-test/4-5-5-6/test'+str(testno),mode='w+')
+    f = open('new-test/1-2-2-3/test'+str(testno),mode='w+')
     f.write("(set-logic ALL)\n")
     f.write("(define-fun-rec pow ((x Int)) Int (ite (= x 0) 1 (* 10 (pow (- x 1)) ) ) )\n")
     f.write(s.sexpr())
--- Benchmark/NIA-test/runall.sh
+++ Benchmark/NIA-test/runall.sh
@@ -2,16 +2,19 @@
 echo "begin test..."
 
 cd new-test
-    cd 2-3-3-4
+    cd 1-2-2-3
     ./runz3.sh > z3-result 
-    ./runcvc4.sh > cvc4-result
     cd ..
-    cd 3-4-4-5
-    ./runz3.sh > z3-result 
-    ./runcvc4.sh > cvc4-result
-    cd ..
-    cd 4-5-5-6
-    ./runz3.sh > z3-result 
-    ./runcvc4.sh > cvc4-result
-    cd ..
+    #cd 2-3-3-4
+    #./runz3.sh > z3-result 
+    #./runcvc4.sh > cvc4-result
+    #cd ..
+    #cd 3-4-4-5
+    #./runz3.sh > z3-result 
+    #./runcvc4.sh > cvc4-result
+    #cd ..
+    #cd 4-5-5-6
+    #./runz3.sh > z3-result 
+    #./runcvc4.sh > cvc4-result
+    #cd ..
 cd ..
