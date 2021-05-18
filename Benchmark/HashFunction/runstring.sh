e19301ef0c70ecd8376e4c7de46675076845df20
EcstasyH <wuhao164@mails.ucas.ac.cn>
2021-05-18 16:10:09 +0800

    exp results

--- Benchmark/HashFunction/runstring.sh
+++ Benchmark/HashFunction/runstring.sh
@@ -3,30 +3,23 @@ echo "begin test..."
 
 cd all
     cd head
-    ./runz3.sh > z3-result 
-    ./runcvc4.sh > cvc4-result
+    ./runz3str.sh > z3str-result 
     cd ..
     cd head-tail
-    ./runz3.sh > z3-result 
-    ./runcvc4.sh > cvc4-result
+    ./runz3str.sh > z3str-result
     cd ..
     cd tail
-    ./runz3.sh > z3-result 
-    ./runcvc4.sh > cvc4-result
+    ./runz3str.sh > z3str-result 
     cd ..
 cd ..
 
 cd flatten
-    cd head
-    ./runz3.sh > z3-result 
-    ./runcvc4.sh > cvc4-result
-    cd ..
     cd head-tail
-    ./runz3.sh > z3-result 
+    ./runz3str.sh > z3str-result 
     ./runcvc4.sh > cvc4-result
     cd ..
     cd tail
-    ./runz3.sh > z3-result 
+    ./runz3str.sh > z3str-result 
     ./runcvc4.sh > cvc4-result
     cd ..
 cd ..
