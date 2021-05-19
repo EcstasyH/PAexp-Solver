b5b19b4073e9b6b821cf08bf24841bf85165f7bc
EcstasyH <wuhao164@mails.ucas.ac.cn>
2021-05-19 11:39:09 +0800

    intro add optimization part

--- Benchmark/HashFunction/runstring.sh
+++ Benchmark/HashFunction/runstring.sh
@@ -2,12 +2,6 @@
 echo "begin test..."
 
 cd all
-    cd head
-    ./runz3str.sh > z3str-result 
-    cd ..
-    cd head-tail
-    ./runz3str.sh > z3str-result
-    cd ..
     cd tail
     ./runz3str.sh > z3str-result 
     cd ..
@@ -16,10 +10,8 @@ cd ..
 cd flatten
     cd head-tail
     ./runz3str.sh > z3str-result 
-    ./runcvc4.sh > cvc4-result
     cd ..
     cd tail
     ./runz3str.sh > z3str-result 
-    ./runcvc4.sh > cvc4-result
     cd ..
 cd ..
